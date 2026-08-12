d05cdea58dadbabe48653a37b0b335183927d478
jphwba <jenishbackup94@gmail.com>
2026-08-12 21:58:49 +0930

    add outw and inw to io

--- src/kernel/pit.h
+++ src/kernel/pit.h
@@ -4,5 +4,11 @@
 
 void pit_init(uint32_t frequency_hz);
 uint32_t pit_get_ticks(void);
+uint32_t pit_get_frequency(void);
+uint32_t pit_get_uptime_ms(void);
+uint32_t pit_get_uptime_seconds(void);
 
+void pit_sleep_ms(uint32_t ms);
+typedef void (*pit_tick_hook_t)(void);
+void pit_set_tick_hook(pit_tick_hook_t hook);
 #endif
