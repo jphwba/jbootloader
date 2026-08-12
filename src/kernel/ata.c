8f082734c44b3672dbe344674c1882f43ffb7d04
jphwba <jenishbackup94@gmail.com>
2026-08-12 21:57:33 +0930

    add ata

---
+++ src/kernel/ata.c
@@ -0,0 +1,56 @@
+#include "ata.h"
+#include "io.h"
+#define ATA_DATA        0x1F0
+#define ATA_ERROR       0x1F1
+#define ATA_SECCOUNT    0x1F2
+#define ATA_LBA_LOW     0x1F3
+#define ATA_LBA_MID     0x1F4
+#define ATA_LBA_HIGH    0x1F5
+#define ATA_DRIVE_HEAD  0x1F6
+#define ATA_STATUS      0x1F7
+#define ATA_COMMAND     0x1F7
+
+#define ATA_CMD_READ_SECTORS 0x20
+
+#define ATA_SR_ERR  0x01
+#define ATA_SR_DRQ  0x08
+#define ATA_SR_BSY  0x80
+
+static int ata_wait_ready(void) {
+    for (int i = 0; i < 100000; i++) {
+        uint8_t status = inb(ATA_STATUS);
+        if (status & ATA_SR_ERR) {
+            return -1;
+        }
+        if (!(status & ATA_SR_BSY) && (status & ATA_SR_DRQ)) {
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int ata_read_sectors(uint32_t lba, uint8_t count, void *buffer) {
+    if (count == 0) {
+        return 0;
+    }
+
+    while (inb(ATA_STATUS) & ATA_SR_BSY) { }
+
+    outb(ATA_DRIVE_HEAD, (uint8_t)(0xE0 | ((lba >> 24) & 0x0F)));
+    outb(ATA_SECCOUNT, count);
+    outb(ATA_LBA_LOW, (uint8_t)(lba & 0xFF));
+    outb(ATA_LBA_MID, (uint8_t)((lba >> 8) & 0xFF));
+    outb(ATA_LBA_HIGH, (uint8_t)((lba >> 16) & 0xFF));
+    outb(ATA_COMMAND, ATA_CMD_READ_SECTORS);
+
+    uint16_t* out = (uint16_t*)buffer;
+    for(uint16_t s = 0; s < count; s++) {
+        if (ata_wait_ready() !=0) {
+            return -1;
+        }
+        for (int i = 0; i < 256; i++) {
+            out[s * 256 + i] = inb(ATA_DATA);
+        }
+    }
+    return 0;
+}
